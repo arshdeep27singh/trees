4ae97abb5911116f5342ba39d090d8db16fe91d1
arshdeep27singh <as536070@gmail.com>
2022-04-06 17:47:02 +0530

    optimised code

--- checkforbalancedtree.cpp
+++ checkforbalancedtree.cpp
@@ -35,3 +35,44 @@ public:
         return false;
     }
 };
+
+// optimised solution of the above
+
+class Solution
+{
+
+public:
+    // Function to check whether a binary tree is balanced or not.
+    pair<bool, int> isBalancedFast(Node *root)
+    {
+        //  Your Code here
+        if (root == NULL)
+        {
+            pair<bool, int> p = make_pair(true, 0);
+            return p;
+        }
+        // return true;
+
+        pair<int, int> left = isBalancedFast(root->left);
+        pair<int, int> right = isBalancedFast(root->right);
+
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+
+        bool diff = abs(left.second - right.second) <= 1;
+
+        pair<bool, int> ans;
+        ans.second = max(left.second, right.second) + 1;
+
+        if (leftAns && rightAns && diff)
+            ans.first = true;
+        else
+            ans.first = false;
+
+        return ans;
+    }
+    bool isBalanced(Node *root)
+    {
+        return isBalancedFast(root).first;
+    }
+};
